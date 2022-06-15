069f3a0c2bc86ba9fb0058804d5cec770fbf950b
Christian Bermeo <christiancabp@gmail.com>
2022-06-15 17:00:15 -0400

    We are hooked. working on the button adopt me feture

--- client/components/AllAnimals.js
+++ client/components/AllAnimals.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { fetchAnimals } from '../store/animals';
-
+import { useSelector, useDispatch } from 'react-redux';
 //The following are for Material-UI components
 import {
   Typography, //text
@@ -13,72 +13,159 @@ import {
   CardMedia,
 } from '@material-ui/core';
 
-class AllAnimals extends React.Component {
-  componentDidMount() {
-    this.props.getAnimals();
-  }
-  render() {
-    return (
-      <div>
-        <Typography variant='h3' component='div'>
-          Welcome to our exotic shelter.
-        </Typography>
-        <ul className='container'>
-          {this.props.animals.length > 0 ? (
-            this.props.animals.map((animal) => (
-              <div className='card' key={animal.id}>
-                <Card>
-                  <Link
-                    to={`animals/${animal.id}`}
-                    style={{ textDecoration: 'none', color: 'black' }}
+const AllAnimals = () => {
+  // pull state from Redux
+  const { animals } = useSelector((state) => {
+    return state;
+  });
+
+  // dispatch actions
+  const dispatch = useDispatch();
+
+  // component did mount
+  useEffect(() => {
+    dispatch(fetchAnimals());
+  }, []);
+
+  return (
+    <div>
+      <Typography variant='h3' component='div'>
+        Welcome to our exotic shelter.
+      </Typography>
+      <ul className='container'>
+        {animals.length > 0 ? (
+          animals.map((animal) => (
+            <div className='card' key={animal.id}>
+              <Card>
+                <Link
+                  to={`animals/${animal.id}`}
+                  style={{ textDecoration: 'none', color: 'black' }}
+                >
+                  <CardMedia
+                    component='img'
+                    alt={`${animal.name} picture`}
+                    image={animal.picture}
+                    height='400'
+                    className='media'
+                  />
+                  <CardContent>
+                    <Typography variant='h5' component='div'>
+                      {animal.name}
+                    </Typography>
+                    <Typography variant='body1' component='div'>
+                      {'$'}
+                      {animal.price}
+                    </Typography>
+                  </CardContent>
+                </Link>
+                <CardActions>
+                  <Button
+                    variant='contained'
+                    color='primary'
+                    size='small'
+                    onClick={() => {
+                      // console.log("button is clicked but nothing happens");
+                    }}
                   >
-                    <CardMedia
-                      component='img'
-                      alt={`${animal.name} picture`}
-                      image={animal.picture}
-                      height='400'
-                      className='media'
-                    />
-                    <CardContent>
-                      <Typography variant='h5' component='div'>
-                        {animal.name}
-                      </Typography>
-                      <Typography variant='body1' component='div'>
-                        {'$'}
-                        {animal.price}
-                      </Typography>
-                    </CardContent>
-                  </Link>
-                  <CardActions>
-                    <Button
-                      variant='contained'
-                      color='primary'
-                      size='small'
-                      onClick={() => {
-                        // console.log("button is clicked but nothing happens");
-                      }}
-                    >
-                      Adopt Me!
-                    </Button>
-                  </CardActions>
-                </Card>
-              </div>
-            ))
-          ) : (
-            <div>Loading Exotic Pets!</div> //this catches while the animals load may not be optimal solution
-          )}
-        </ul>
-      </div>
-    );
-  }
-}
+                    Adopt Me!
+                  </Button>
+                </CardActions>
+              </Card>
+            </div>
+          ))
+        ) : (
+          <div>Loading Exotic Pets!</div> //this catches while the animals load may not be optimal solution
+        )}
+      </ul>
+    </div>
+  );
+};
 
-const mapStateToProps = (state) => ({
-  animals: state.animals,
-});
+export default AllAnimals;
 
-const mapDispatchToProps = (dispatch) => ({
-  getAnimals: () => dispatch(fetchAnimals()),
-});
+// Prior to React Hooks
 
-export default connect(mapStateToProps, mapDispatchToProps)(AllAnimals);
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { Link } from 'react-router-dom';
+// import { fetchAnimals } from '../store/animals';
+
+// //The following are for Material-UI components
+// import {
+//   Typography, //text
+//   Button,
+//   Card,
+//   CardActions,
+//   CardContent,
+//   CardMedia,
+// } from '@material-ui/core';
+
+// class AllAnimals extends React.Component {
+//   componentDidMount() {
+//     this.props.getAnimals();
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <Typography variant='h3' component='div'>
+//           Welcome to our exotic shelter.
+//         </Typography>
+//         <ul className='container'>
+//           {this.props.animals.length > 0 ? (
+//             this.props.animals.map((animal) => (
+//               <div className='card' key={animal.id}>
+//                 <Card>
+//                   <Link
+//                     to={`animals/${animal.id}`}
+//                     style={{ textDecoration: 'none', color: 'black' }}
+//                   >
+//                     <CardMedia
+//                       component='img'
+//                       alt={`${animal.name} picture`}
+//                       image={animal.picture}
+//                       height='400'
+//                       className='media'
+//                     />
+//                     <CardContent>
+//                       <Typography variant='h5' component='div'>
+//                         {animal.name}
+//                       </Typography>
+//                       <Typography variant='body1' component='div'>
+//                         {'$'}
+//                         {animal.price}
+//                       </Typography>
+//                     </CardContent>
+//                   </Link>
+//                   <CardActions>
+//                     <Button
+//                       variant='contained'
+//                       color='primary'
+//                       size='small'
+//                       onClick={() => {
+//                         // console.log("button is clicked but nothing happens");
+//                       }}
+//                     >
+//                       Adopt Me!
+//                     </Button>
+//                   </CardActions>
+//                 </Card>
+//               </div>
+//             ))
+//           ) : (
+//             <div>Loading Exotic Pets!</div> //this catches while the animals load may not be optimal solution
+//           )}
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => ({
+//   animals: state.animals,
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   getAnimals: () => dispatch(fetchAnimals()),
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(AllAnimals);
--- client/components/SingleAnimal.js
+++ client/components/SingleAnimal.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { fetchAnimal } from '../store/animal';
-
-//The following are for Material-UI components
+// The following are for Material-UI components
 import {
   Typography, //text
   Button,
@@ -12,74 +12,160 @@ import {
   CardActions,
 } from '@material-ui/core';
 
-class SingleAnimal extends React.Component {
-  componentDidMount() {
-    this.props.getAnimal(this.props.match.params.id);
-  }
+const SingleAnimal = (props) => {
+  // pull state from redux
+  const { animal } = useSelector((state) => {
+    return state;
+  });
 
-  render() {
-    const { animal } = this.props;
+  // dispatch actions
+  const dispatch = useDispatch();
 
-    return (
-      <div className='container'>
-        <Card>
-          <CardMedia
-            component='img'
-            alt={`${animal.name} picture`}
-            image={animal.picture}
-            height='500'
-            width='80'
-            className='media'
-          />
-          <CardContent>
-            <Typography variant='h5' component='div'>
-              {animal.name}
-            </Typography>
-            <Typography variant='body1' component='div'>
-              Latin Name: {animal.latinName}
-            </Typography>
-            <Typography variant='body1' component='div'>
-              Type: {animal.animalType}
-            </Typography>
-            <Typography variant='body1' component='div'>
-              Diet: {animal.diet}
-            </Typography>
-            <Typography variant='body1' component='div'>
-              Habitat: {animal.habitat}
-            </Typography>
-            <Typography variant='body1' component='div'>
-              Location: {animal.location}
-            </Typography>
-            <Typography variant='body1' component='div'>
-              LifeSpan: {animal.lifeSpan} years!
-            </Typography>
-            <Typography variant='body1' component='div'>
-              Price: ${animal.price}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button
-              variant='contained'
-              color='primary'
-              size='small'
-              onClick={() => {
-                // console.log("button is clicked but nothing happens");
-              }}
-            >
-              Adopt Me!
-            </Button>
-          </CardActions>
-        </Card>
-      </div>
-    );
-  }
-}
+  // component did mount
+  useEffect(() => {
+    dispatch(fetchAnimal(props.match.params.id));
+  }, []);
 
-const mapStateToProps = (state) => ({
-  animal: state.animal,
-});
-const mapDispatchToProps = (dispatch) => ({
-  getAnimal: (id) => dispatch(fetchAnimal(id)),
-});
+  return (
+    <div className='container'>
+      <Card>
+        <CardMedia
+          component='img'
+          alt={`${animal.name} picture`}
+          image={animal.picture}
+          height='500'
+          width='80'
+          className='media'
+        />
+        <CardContent>
+          <Typography variant='h5' component='div'>
+            {animal.name}
+          </Typography>
+          <Typography variant='body1' component='div'>
+            Latin Name: {animal.latinName}
+          </Typography>
+          <Typography variant='body1' component='div'>
+            Type: {animal.animalType}
+          </Typography>
+          <Typography variant='body1' component='div'>
+            Diet: {animal.diet}
+          </Typography>
+          <Typography variant='body1' component='div'>
+            Habitat: {animal.habitat}
+          </Typography>
+          <Typography variant='body1' component='div'>
+            Location: {animal.location}
+          </Typography>
+          <Typography variant='body1' component='div'>
+            LifeSpan: {animal.lifeSpan} years!
+          </Typography>
+          <Typography variant='body1' component='div'>
+            Price: ${animal.price}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant='contained'
+            color='primary'
+            size='small'
+            onClick={() => {
+              // console.log("button is clicked but nothing happens");
+            }}
+          >
+            Adopt Me!
+          </Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(SingleAnimal);
+export default SingleAnimal;
+
+// Prior to hooks
+
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { fetchAnimal } from '../store/animal';
+
+// //The following are for Material-UI components
+// import {
+//   Typography, //text
+//   Button,
+//   Card,
+//   CardMedia,
+//   CardContent,
+//   CardActions,
+// } from '@material-ui/core';
+
+// class SingleAnimal extends React.Component {
+//   componentDidMount() {
+//     this.props.getAnimal(this.props.match.params.id);
+//   }
+
+//   render() {
+//     const { animal } = this.props;
+
+//     return (
+//       <div className='container'>
+//         <Card>
+//           <CardMedia
+//             component='img'
+//             alt={`${animal.name} picture`}
+//             image={animal.picture}
+//             height='500'
+//             width='80'
+//             className='media'
+//           />
+//           <CardContent>
+//             <Typography variant='h5' component='div'>
+//               {animal.name}
+//             </Typography>
+//             <Typography variant='body1' component='div'>
+//               Latin Name: {animal.latinName}
+//             </Typography>
+//             <Typography variant='body1' component='div'>
+//               Type: {animal.animalType}
+//             </Typography>
+//             <Typography variant='body1' component='div'>
+//               Diet: {animal.diet}
+//             </Typography>
+//             <Typography variant='body1' component='div'>
+//               Habitat: {animal.habitat}
+//             </Typography>
+//             <Typography variant='body1' component='div'>
+//               Location: {animal.location}
+//             </Typography>
+//             <Typography variant='body1' component='div'>
+//               LifeSpan: {animal.lifeSpan} years!
+//             </Typography>
+//             <Typography variant='body1' component='div'>
+//               Price: ${animal.price}
+//             </Typography>
+//           </CardContent>
+//           <CardActions>
+//             <Button
+//               variant='contained'
+//               color='primary'
+//               size='small'
+//               onClick={() => {
+//                 // console.log("button is clicked but nothing happens");
+//               }}
+//             >
+//               Adopt Me!
+//             </Button>
+//           </CardActions>
+//         </Card>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => ({
+//   animal: state.animal,
+// });
+// const mapDispatchToProps = (dispatch) => ({
+//   getAnimal: (id) => dispatch(fetchAnimal(id)),
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(SingleAnimal);
