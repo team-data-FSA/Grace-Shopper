fc4bc636510c5df902ff0083be3815c41e556732
ColinKetcham <42621093+ColinKetcham@users.noreply.github.com>
2022-06-14 22:04:20 -0700

    Revert "React components"

--- client/Routes.js
+++ client/Routes.js
@@ -1,71 +1,63 @@
-
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
-import { withRouter, Route, Switch, Redirect } from "react-router-dom";
-import { Login, Signup } from "./components/AuthForm";
-import Home from "./components/Home";
-import AllAnimals from "./components/AllAnimals";
-import SingleAnimal from "./components/SingleAnimal";
-import { me } from "./store";
-
+import React, {Component, Fragment} from 'react'
+import {connect} from 'react-redux'
+import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
+import { Login, Signup } from './components/AuthForm';
+import Home from './components/Home';
+import Checkout from './components/Checkout';
+import Confirmation from './components/Confirmation';
+import {me} from './store'
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
   componentDidMount() {
-    this.props.loadInitialData();
+    this.props.loadInitialData()
   }
 
   render() {
-    const { isLoggedIn } = this.props;
+    const {isLoggedIn} = this.props
 
     return (
       <div>
         {isLoggedIn ? (
           <Switch>
-            <Route path="/animals" exact component={AllAnimals} />
-            <Route path="/animals/:id" exact component={SingleAnimal} />
-
-            {/* I commented this out to be able to see my AllAnimals component */}
-            {/* <Route path="/home" component={Home} /> */}
-            <Redirect to="/animals" />
-//             <Route path='/checkout' component={Checkout} /> //not sure which of these we need. kept after conflict.
-//             <Route path='/confirmed' component={Confirmation} />
-//             <Redirect to="/home" />
-
+            <Route path="/home" component={Home} />
+            <Route path='/checkout' component={Checkout} />
+            <Route path='/confirmed' component={Confirmation} />
+            <Redirect to="/home" />
           </Switch>
         ) : (
           <Switch>
-            <Route path="/" exact component={Login} />
+            <Route path='/' exact component={ Login } />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
           </Switch>
         )}
       </div>
-    );
+    )
   }
 }
 
 /**
  * CONTAINER
  */
-const mapState = (state) => {
+const mapState = state => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id,
-  };
-};
+    isLoggedIn: !!state.auth.id
+  }
+}
 
-const mapDispatch = (dispatch) => {
+const mapDispatch = dispatch => {
   return {
     loadInitialData() {
-      dispatch(me());
-    },
-  };
-};
+      dispatch(me())
+    }
+  }
+}
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes));
+export default withRouter(connect(mapState, mapDispatch)(Routes))
